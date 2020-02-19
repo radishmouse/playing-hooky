d61e4553b1e44edd38502ec298fc9189071ab0d2
Chris Aquino <chrisaquino@radishmouse.local>
2020-02-19 14:34:08 -0500

    adds example of redirecting if empty count

--- src/OutputComponent.js
+++ src/OutputComponent.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 
 export default function Output({
     count
 }) {
-    return (
-        <div>
-            <h1>Your count:</h1>        
-            <ul>
-            {
-                Object.keys(count).map(dateStamp => {
-                return <li>{dateStamp}: {count[dateStamp]}</li>    
-                })
-            }
-            </ul>
-        </div>
-    )
+    let history = useHistory();
+    if (Object.keys(count).length === 0) {
+        history.push('/increment');
+        return <div></div>
+    } else {
+
+        
+            return (
+                <div>
+                    <h1>Your count:</h1>        
+                    <ul>
+                    {
+                        Object.keys(count).map(dateStamp => {
+                        return <li>{dateStamp}: {count[dateStamp]}</li>    
+                        })
+                    }
+                    </ul>
+                </div>
+            )
+    }
 }
